4da9dbbb1ea2d286c1ab8b7a73d42562cfc0263f
noeticwxb <noeticwxb@gmail.com>
2016-06-29 10:27:01 +0800

    Init

---
+++ app/src/main/java/com/example/wangxingbo/lockscreen/AdminReceiver.java
@@ -0,0 +1,10 @@
+package com.example.wangxingbo.lockscreen;
+
+/**
+ * Created by wangxingbo on 2016/6/28.
+ */
+
+import android.app.admin.DeviceAdminReceiver;
+
+public class AdminReceiver extends  DeviceAdminReceiver {
+}
